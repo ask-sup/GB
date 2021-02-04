1045528c45cf7a4f9b2a16cad854f0bad0845b08
ask.sup <askelawd@gmail.com>
2021-02-04 23:34:05 +0300

    very first sctipt refactored

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function Calc() {
-    var firstDigit = prompt("Введите первое число для сложения");
-    var secondDigit = prompt("Введите второе число для сложения");
+    let firstDigit = prompt("Введите первое число для сложения");
+    let secondDigit = prompt("Введите второе число для сложения");
     firstDigit = parseInt(firstDigit, 10);
     secondDigit = parseInt(secondDigit, 10);
-    var calcResult = firstDigit + secondDigit;
-    alert("Результат сложения: " + calcResult);
-};
+    let calcResult = firstDigit + secondDigit;
+    alert(`Результат сложения: ${calcResult}`);
+}
 
 Calc();
